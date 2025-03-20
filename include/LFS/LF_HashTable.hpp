092b53e3af14c7bb8f06aae52753f580208583ca
7Zon2 <manukovskiy.1997@gmail.com>
2025-03-20 06:06:55 +0300

    new LFS files

---
+++ include/LFS/LF_HashTable.hpp
@@ -0,0 +1,678 @@
+#pragma once 
+#include "LFS/share_resource.hpp"
+#include "LF_List.hpp"
+#include <algorithm>
+#include <atomic>
+#include <cmath>
+#include <math.h>
+#include <byteswap.h>
+#include <optional>
+
+[[nodiscard]]
+size_t 
+constexpr inline reverse_bit_order
+(size_t x) noexcept 
+{
+  return bswap_64(x);
+}
+
+
+[[nodiscard]]
+constexpr size_t
+inline msb
+(size_t value) noexcept 
+{
+  return 64 - __builtin_clzll(value);
+}
+
+
+template<typename T>
+struct HashNode
+{
+  bool isDummy = true;
+  T data_;
+};
+
+
+template<typename T>
+requires requires(T&& data)
+{
+  typename T::first_type;
+  typename T::second_type;
+}
+struct std::hash<HashNode<T>>
+{
+  constexpr 
+  size_t operator()(const HashNode<T>& node) const noexcept
+  {
+    return std::hash<typename T::first_type>{}(node.data_.first);
+  }
+};
+
+
+template<typename T>
+requires requires(T&& data)
+{
+  std::hash<T>{}(data);
+}
+struct std::hash<HashNode<T>>
+{
+  constexpr 
+  size_t operator()(const HashNode<T>& node) const noexcept 
+  {
+    return std::hash<T>{data};
+  }
+};
+
+
+template<typename Key, typename Obj>
+class LF_HashTable
+{
+  public:
+
+  using comporator_type = decltype([](const size_t lhs, const size_t rhs) noexcept
+  {
+    return reverse_bit_order(lhs) <= reverse_bit_order(rhs);
+  });
+
+  using pair_t = std::pair<Key, Obj>;
+  using HashNode_t = HashNode<pair_t>;
+  using List_t = LF_OrderList<HashNode_t, comporator_type>;
+  using Node = typename List_t::Node;
+
+  protected:
+
+  class HT_iterator : private List_t::iterator
+  {
+    protected:
+
+    using it_t = typename List_t::iterator;
+    using it_t::node_;
+
+    public:
+
+    HT_iterator(){}
+
+    HT_iterator(it_t it):
+      it_t(it){}
+
+    friend bool operator == (const HT_iterator& it, const Node* node) noexcept
+    {
+      return it.node_;
+    }
+
+    friend bool operator == (const HT_iterator&, const HT_iterator&) noexcept = default;
+
+    public:
+
+    using iterator_category = typename it_t::iterator_category;
+    using value_type = pair_t;
+    using pointer = pair_t*;
+    using reference = pair_t&;
+    using difference_type = typename it_t::difference_type;
+    
+    public:
+
+    HT_iterator& operator ++() noexcept
+    {
+      it_t::operator++();
+      return *this;
+    }
+
+
+    HT_iterator operator ++(int) noexcept
+    {
+      HT_iterator temp{*this};
+      it_t::operator++();
+      return temp;
+    }
+
+
+    reference operator*() const noexcept
+    {
+      HashNode_t& node = it_t::operator*();
+      return node.data_;
+    }
+
+
+    pointer operator->() const noexcept
+    {
+      HashNode_t& node =  it_t::operator*();
+      return &node.data_;
+    }
+  };
+
+  static_assert(std::forward_iterator<HT_iterator>);
+
+  protected:
+
+  class Bucket
+  {
+    std::atomic<Node*> bucket_head_;
+    std::atomic<size_t> bucket_count_;
+    size_t bucket_hash_;
+
+    public:
+      
+      using iterator = List_t::iterator;
+
+      Bucket(Node* node, size_t hash) noexcept:
+        bucket_head_(node),
+        bucket_count_(0),
+        bucket_hash_(hash)
+        {}
+
+
+      [[nodiscard]]
+      Node* 
+      get() const noexcept
+      {
+        return bucket_head_.load(std::memory_order_relaxed);
+      }
+
+      [[nodiscard]]
+      size_t 
+      count() const noexcept
+      {
+        return bucket_count_.load(std::memory_order_relaxed);
+      }
+
+      void 
+      reset_count() noexcept
+      {
+        bucket_count_.store(0, std::memory_order_relaxed);
+      }
+
+      [[nodiscard]]
+      size_t 
+      hash() const noexcept
+      {
+        return bucket_hash_;
+      }
+
+      bool insert(List_t& list_, HashNode_t hashNode)
+      {
+        bool before = true;
+        Node* node = list_.insert(std::move(hashNode), bucket_head_.load(std::memory_order_relaxed), before);
+        bucket_count_.fetch_add(static_cast<bool>(node), std::memory_order_release); 
+        return node;
+      }
+
+      bool remove(List_t& list_, const Key& key)
+      {
+        bool res = list_.remove(key, bucket_head_.load(std::memory_order_relaxed));
+        bucket_count_.fetch_sub(res, std::memory_order_release);
+        return res;
+      }
+  };
+
+  protected:
+
+  List_t list_;
+  size_t load_factor_;
+  std::atomic<int> counter_; 
+
+  using segment = std::pmr::vector<std::atomic<Bucket*>>;
+  std::array<std::atomic<segment*>, 64> seg_arr_;
+  std::atomic<size_t> current_segment_;
+
+  protected:
+
+  [[nodiscard]]
+  segment* 
+  get_segment(const size_t segment_index) const noexcept
+  {
+    segment * seg;
+    do
+    {
+      seg = seg_arr_[segment_index].load(std::memory_order_relaxed);
+    }while(!seg);// in the inflate method this segment may be not allocated yet
+    
+    return seg;
+  }
+
+
+  [[nodiscard]]
+  size_t
+  get_hash(const Key& key) const noexcept 
+  {
+    return std::hash<Key>{}(key);
+  }
+
+
+  [[nodiscard]]
+  size_t 
+  get_index
+  (const size_t hash, size_t segment_index) const noexcept
+  {
+    size_t table_index = get_table_index(hash, segment_index);
+    segment_index = get_table_segment(table_index);
+    size_t table_size = get_table_size(segment_index);
+    size_t offset = get_table_offset(table_index);
+    size_t index = table_size - offset -1;
+    return index;
+  }
+
+
+  [[nodiscard]]
+  size_t 
+  get_index
+  (const size_t table_index)
+  {
+    return get_index(table_index, current_segment_.load(std::memory_order_relaxed));
+  }
+
+
+  [[nodiscard]]
+  segment& get_table
+  (const size_t hash,size_t segment_index) const noexcept
+  {
+    size_t table_index = get_table_index(hash, segment_index);
+    segment_index = get_table_segment(table_index);
+    return *get_segment(segment_index);
+  }
+
+  
+  [[nodiscard]]
+  segment& get_table
+  (size_t table_index) const noexcept 
+  {
+    return get_table(table_index, current_segment_.load(std::memory_order_relaxed));
+  }
+
+
+  [[nodiscard]]
+  Bucket* 
+  get_parent_bucket
+  (const size_t modulo, size_t segment_index) noexcept
+  {
+    struct LocalIndexes
+    {
+      int parent_index = 0;
+      int segment_index = 0;
+    };
+
+    auto find_parent_bucket = [&](size_t parent_index, size_t segment_index)
+    {
+      auto& table = get_table(parent_index, segment_index);
+      size_t index = get_index(parent_index, segment_index);
+      Bucket* bucket = table[index].load(std::memory_order_relaxed);
+      return bucket;
+    };
+
+    std::vector<LocalIndexes> vec;
+
+    auto& table = get_table(modulo, segment_index);
+    size_t index = get_index(modulo, segment_index);
+    Bucket* bucket = table[index].load(std::memory_order_relaxed);
+    
+    vec.push_back(LocalIndexes(modulo, segment_index));
+    
+    int parent_index = modulo;
+    while(!bucket) //search for the first initialized bucket
+    {
+      parent_index = get_table_index(parent_index, segment_index);
+      bucket = find_parent_bucket(parent_index, --segment_index);
+      vec.push_back(LocalIndexes(parent_index, segment_index));
+    }
+
+    assert(parent_index >= 0 && segment_index >=0); // the first bucket must be always initialized
+
+    for(auto indexes : vec | std::views::reverse) // initializes all new parent buckets with their previous parent. 
+                                                   // if previous parent equal the next parent then nothing happens
+    {
+      parent_index = indexes.parent_index;
+      segment_index = indexes.segment_index;
+      auto& table = get_table(parent_index);
+      index = get_index(parent_index);
+      bucket = insert_parent_bucket(table, segment_index, index, parent_index, bucket);
+    }
+
+    assert(bucket && parent_index == modulo);
+    return bucket;
+  }
+
+
+  [[nodiscard]]
+  Bucket* 
+  insert_parent_bucket
+  (segment& table, const size_t segment_index, const size_t index, const size_t parent_hash, Bucket* parent)
+  {
+    Bucket* old_bucket = table[index].load(std::memory_order_acquire);
+    if(old_bucket) //if the parent bucket is already in the table - it is already in the list 
+    {
+      return old_bucket;
+    }
+
+    Node* insert_node{};
+    
+    bool before = true; // we need to insert a new node before another one 
+                        // that we will find (due to order list insertion) 
+    while(!insert_node)
+    {
+      insert_node = list_.insert(HashNode_t{true, pair_t{parent_hash, Obj{}}}, parent->get(), before);
+    }
+
+    Bucket * new_bucket = new Bucket(insert_node, parent_hash);
+
+    //if two or more threads inserted the same node we need to remove redundant
+    if(!table[index].compare_exchange_strong
+        (old_bucket, new_bucket, std::memory_order_release, std::memory_order_relaxed))
+    {
+      delete new_bucket;
+      Bucket* pb = get_parent_bucket(parent_hash, segment_index - 1); 
+      list_.remove(parent_hash, pb->get());
+    }
+
+    return table[index].load(std::memory_order_relaxed);
+  }
+
+
+  [[nodiscard]]
+  size_t 
+  get_table_size() const noexcept
+  {    
+    return get_table_size(current_segment_.load(std::memory_order_relaxed));
+  }
+
+
+  [[nodiscard]]
+  size_t 
+  get_table_size
+  (const size_t segment_index) const noexcept 
+  {
+    return 1 << segment_index;
+  }
+
+
+  [[nodiscard]]
+  size_t
+  get_table_index
+  (const size_t hash, const size_t segment_index) const noexcept 
+  {
+    return hash % get_table_size(segment_index);
+  }
+
+
+  [[nodiscard]]
+  size_t
+  get_table_segment
+  (const size_t table_index) const noexcept
+  {
+    if(table_index == 0)
+    {
+      return 0;
+    }
+    if(table_index == 1)
+    {
+      return 1;
+    }
+    return std::ceil(std::log2(table_index));
+  }
+
+
+  [[nodiscard]]
+  size_t 
+  get_table_offset
+  (const size_t table_index) const noexcept
+  {
+    if(table_index == 0)
+    {
+      return 0;
+    }
+    size_t segment_index = get_table_segment(table_index);
+    size_t table_size = get_table_size(segment_index);
+    size_t dif = table_size - table_index;
+    return dif;
+  }
+
+
+  [[nodiscard]]
+  bool
+  check_load_factor() const noexcept
+  {
+    size_t sz = get_table_size();
+    return (list_.size() + 1) / sz > load_factor_;
+  }
+
+
+  [[nodiscard]]
+  bool
+  check_load_factor(const size_t sz) const noexcept
+  {
+    return list_.size() /sz > load_factor_;
+  }
+
+
+  bool 
+  inflate_table()
+  {
+    size_t old_index = current_segment_.load(std::memory_order_relaxed);
+    if(!check_load_factor())
+    {
+      return false;
+    }
+    
+    if(!current_segment_.compare_exchange_strong(old_index, old_index+1, std::memory_order_relaxed))
+    {
+      return false;
+    }
+    
+    size_t new_index = old_index + 1;
+    size_t table_size = get_table_size(new_index);
+    segment* table = seg_arr_[new_index].load(std::memory_order_acquire);
+    assert(!table);
+    table = new segment(table_size, &ShareResource::res_);
+    seg_arr_[new_index].store(table, std::memory_order_release);
+    return true;
+  }
+
+  public:
+
+  using iterator = HT_iterator;
+
+  LF_HashTable(const size_t load_factor = 2):
+    load_factor_(load_factor), current_segment_(0)
+  {
+    segment  * table = seg_arr_[0].load(std::memory_order_acquire);
+    table = new segment(1, &ShareResource::res_);
+
+    pair_t pa{Key{},Obj{}};
+    HashNode_t hash_node{true, std::move(pa)};
+
+    Node* node = list_.insert(hash_node);
+    node->data_.first = 0;
+
+    Bucket* new_bucket = new Bucket(node, 0);
+
+    segment& vec = *table;
+    vec[0].store(new_bucket, std::memory_order_relaxed);
+
+    seg_arr_[0].store(table, std::memory_order_release);
+  }
+
+
+  virtual 
+  ~LF_HashTable()
+  {
+    segment* seg = seg_arr_[0].exchange(nullptr,std::memory_order_relaxed);
+    if(!seg)
+    {
+      return;
+    }
+   
+    segment& table = *seg;
+    for(size_t i = 0; i < table.size(); i++)
+    {
+      Bucket* bucket = table[i].load(std::memory_order_relaxed);
+      if(bucket)
+      {
+        delete bucket;
+      }
+    }
+
+    delete seg;
+    size_t sz = seg_arr_.size();
+
+    for(size_t i = 1; i <sz; i++)
+    {
+      seg = seg_arr_[i].load(std::memory_order_relaxed);
+      if(seg)
+      {
+        segment& table = *seg;
+        for(size_t j = 0; j < table.size(); j++)
+        {
+          Bucket* bucket = table[j].load(std::memory_order_relaxed);
+          if(bucket)
+          {
+            delete bucket;
+          }
+        }
+        delete seg;
+      }
+    }
+  }
+
+  public:
+
+  [[nodiscard]]
+  size_t size() const noexcept
+  {
+    return counter_.load(std::memory_order_acquire);
+  }
+
+
+  template<typename T, typename U>
+  bool insert(T&& key, U&& value)
+  {
+    if(find(key))
+    {
+      return false;
+    }
+
+    size_t hash  = get_hash(key);
+
+    inflate_table();
+    Bucket* parent_bucket = get_parent_bucket(hash, current_segment_.load(std::memory_order_relaxed));
+    
+    pair_t pa{std::forward<T>(key), std::forward<U>(value)};
+    HashNode_t hashNode{false, std::move(pa)};
+
+    bool res = parent_bucket->insert(list_, std::move(hashNode));
+    counter_.fetch_add(res, std::memory_order_release);
+    return res;
+  }
+
+
+  bool remove(const Key& key)
+  {
+    size_t hash = get_hash(key);
+    size_t segment_index = current_segment_.load(std::memory_order_relaxed);
+
+    Bucket * bucket = get_parent_bucket(hash, segment_index);
+    if(!bucket)
+    {
+      return {};
+    }
+
+    bool res = bucket->remove(list_, key);
+    counter_.fetch_sub(res, std::memory_order_release);
+    return res;
+  }
+
+
+  [[nodiscard]]
+  bool 
+  find(const Key& key)
+  {
+    size_t hash = get_hash(key);
+    size_t segment_index = current_segment_.load(std::memory_order_relaxed);
+    
+    Bucket* bucket = get_parent_bucket(hash, segment_index);
+    if(!bucket)
+    {
+      return {};
+    }
+
+    std::optional<HashNode_t> opt = list_.contain(key, bucket->get());
+    if(opt.has_value() && (opt.value().isDummy == false))
+    {
+      return true;
+    }
+    return false;
+  }
+
+
+  [[nodiscard]]
+  std::optional<Obj> 
+  contain(const Key& key)
+  {
+    size_t hash = get_hash(key);
+    size_t segment_index = current_segment_.load(std::memory_order_relaxed);
+    
+    Bucket* bucket = get_parent_bucket(hash, segment_index);
+    if(!bucket)
+    {
+      return {};
+    }
+
+    std::optional<HashNode<pair_t>> opt = list_.contain(hash, bucket->get());
+    if(opt.has_value() && (opt.value().isDummy==false))
+    {
+      HashNode_t& hn = opt.value();
+      return hn.data_.second;
+    }
+    return {};
+  }
+
+ 
+  void clear()
+  {
+    for(auto& node: list_)
+    {
+      const Key& key = node.data_.first;
+      remove(key);
+    }
+    
+
+    /*for(std::atomic<segment*>& ptr : seg_arr_ | std::views::reverse)
+    {
+      segment* seg = ptr.load(std::memory_order_relaxed);
+      if(!seg)
+      {
+        continue;
+      }
+
+      segment& table = *seg;
+      for(std::atomic<Bucket*>& anode : table | std::views::reverse)
+      {
+        Bucket* bucket = anode.load(std::memory_order_relaxed);
+        if(!bucket)
+        {
+          continue;
+        }
+        bucket->reset_count();
+      }
+    }*/
+    counter_.store(0, std::memory_order_release);
+  }
+
+
+  Bucket* get_bucket(const Key& key) 
+  {
+    size_t hash = get_hash(key);
+    return get_parent_bucket(hash, current_segment_.load(std::memory_order_relaxed));
+  }
+
+
+  auto begin() noexcept
+  {
+    return HT_iterator(list_.begin());
+  }
+
+  
+  auto end() noexcept
+  {
+    return HT_iterator(list_.end());
+  }
+};
---
+++ include/LFS/LF_List.hpp
@@ -0,0 +1,507 @@
+#pragma once
+#include "LFS/LF_FreeList.hpp"
+#include "LFS/LF_hazardous.hpp"
+#include "LF_allocator.hpp"
+#include <atomic>
+#include <functional>
+#include <iterator>
+#include <memory>
+#include <type_traits>
+#include <utility>
+
+
+template<typename T, typename Compare = std::less_equal<T>>
+class LF_OrderList : protected FreeList<std::pair<size_t,T>, true>
+{
+  public:
+
+  using type = std::pair<size_t, T>;
+  using FreeList_t = FreeList<type, true>;
+  
+  using typename FreeList_t::Node;
+  using typename FreeList_t::size;
+  
+  protected:
+
+  using FreeList_t::head_;
+  using FreeList_t::counter_;
+  using FreeList_t::tail_;
+  using FreeList_t::allocate;
+  using FreeList_t::begin;
+  using FreeList_t::end;
+  
+  protected:
+
+  class order_iterator : private FreeList_t::iterator
+  {
+    protected:
+
+    using it_t = FreeList_t::iterator; 
+    using it_t::node_;
+
+    public:
+
+    order_iterator(){}
+    
+    order_iterator(Node* node):
+      FreeList_t::iterator(node){}
+
+    
+    friend bool operator == (const order_iterator& it, const Node* node) noexcept
+    {
+      return it.node_ == node;
+    }
+
+    friend bool operator ==(const order_iterator&, const order_iterator&) noexcept = default; 
+
+    public:
+
+    using iterator_category = typename it_t::iterator_category;
+    using value_type = std::remove_cvref_t<T>;
+    using pointer =  T*;
+    using reference = T&;
+    using difference_type = typename it_t::difference_type;
+
+    public:
+
+    order_iterator& operator++() noexcept
+    {
+      Node* next = node_->next();
+      next = clear_tag(next).second;
+      node_ = next;
+      return *this;
+    }
+
+
+    order_iterator operator++(int) noexcept
+    {
+      order_iterator temp{*this};
+      operator++();
+      return temp;
+    }
+
+
+    reference operator*() const noexcept
+    {
+      type& pa = it_t::operator*();
+      return pa.second;
+    }
+
+
+    pointer operator->() const noexcept
+    {
+      type& pa = it_t::operator*();
+      return &pa.second;
+    }
+
+  };
+
+  static_assert(std::forward_iterator<order_iterator>);
+
+  protected:
+
+  LF_allocator alloc_;
+  Compare comporator_;
+
+  public:
+
+  using iterator = order_iterator;
+
+  LF_OrderList(Compare comporator = Compare{}):
+    alloc_([this](void* data, size_t data_size){
+        if(data)
+          std::destroy_at(static_cast<type*>(data));
+    }), 
+    comporator_(comporator)
+  {
+    FreeList<type,true>::set_allocator(&alloc_);
+    void* p = allocate();
+    Node* next = ::new(p) Node(); //the ghost node for the find method
+    Node* head = head_.load(std::memory_order_relaxed);
+    head->next_.store(next, std::memory_order_relaxed);
+    tail_.store(next, std::memory_order_relaxed);
+  }
+
+
+  LF_OrderList(LF_OrderList&& rhs) noexcept
+  {
+    LF_OrderList temp;
+    head_ =  rhs.head_.exchange(temp.head_, std::memory_order_acq_rel);
+    tail_  = rhs.tail_.exchange(temp.tail_, std::memory_order_acq_rel);
+    comporator_ = std::move(rhs.comporator_);
+    alloc_ = std::move(rhs.alloc_);
+    temp.reset();
+  }
+
+
+  LF_OrderList& operator = (LF_OrderList&& rhs) noexcept
+  {
+    if(this!=&rhs)
+    {
+      LF_OrderList temp;
+      head_ =  rhs.head_.exchange(temp.head_, std::memory_order_acq_rel);
+      tail_  = rhs.tail_.exchange(temp.tail_, std::memory_order_acq_rel);
+      comporator_ = std::move(rhs.comporator_);
+      alloc_ = std::move(temp.alloc_);
+      temp.reset();
+    }
+    return *this;
+  }
+
+
+  ~LF_OrderList()
+  {
+    clear();
+    Node* head = head_.exchange(nullptr, std::memory_order_relaxed);
+    while(head)
+    {
+      Node* next = head->next();
+      alloc_.deallocate(head, sizeof(Node));
+      head = next;
+    }
+  }
+
+  protected:
+
+  [[nodiscard]]
+  static Node* 
+  corrupt_node
+  (Node * ptr) noexcept
+  {
+    return reinterpret_cast<Node*>(reinterpret_cast<size_t>(ptr)|1);
+  }
+
+
+  [[nodiscard]]
+  static bool 
+  is_tagged 
+  (Node* ptr)  noexcept 
+  {
+    return reinterpret_cast<size_t>(ptr) & 1; 
+  }
+
+
+  [[nodiscard]]
+  static auto
+  clear_tag
+  (Node* ptr) noexcept
+  {
+    bool Tagged = is_tagged(ptr);
+    if(Tagged)
+      ptr = reinterpret_cast<Node*>(reinterpret_cast<size_t>(ptr) ^1);
+
+    return std::make_pair(Tagged, ptr);
+  }
+
+  protected:
+
+  [[nodiscard]]
+  std::pair<Node*, Hazardous::HZP*>
+  search_node(size_t key, Node* it = {})
+  {
+    if(!it)
+    {
+      it = head_.load(std::memory_order_relaxed);
+    }
+
+    Node * prev = it;
+    Node * curr = nullptr;
+    Node * next = nullptr;
+    auto hzp_c = alloc_.get_hazard();
+    for(;;)
+    {
+      curr = prev->next();
+      if(!curr)
+      {
+        hzp_c->reset();
+        return std::make_pair(prev, nullptr);
+      }
+
+      auto pc = clear_tag(curr);
+      Node * cleared = pc.second;
+      bool marked = pc.first;
+
+      hzp_c->protect(cleared);
+      if(curr != prev->next())
+      {
+        prev = head_.load(std::memory_order_relaxed);
+        continue;
+      }
+
+
+      while(marked)
+      {
+        if(cleared)
+        {
+          next = cleared->next();
+        }
+        else
+        {
+          next = nullptr;
+        }
+
+        bool success = prev->next_.compare_exchange_strong(curr, next, std::memory_order_relaxed);
+        if(success)
+        {
+          alloc_.deallocate_hazard(hzp_c);
+          hzp_c = alloc_.get_hazard();
+        }
+        else
+        {
+          prev = head_.load(std::memory_order_relaxed);
+          curr = nullptr;
+          break;
+        }
+
+        curr = next; //next may be corrupted or nullptr
+        pc = clear_tag(curr);
+        marked = pc.first;
+        cleared = pc.second;
+
+        hzp_c->protect(cleared);
+        if(prev->next()!=next)
+        {
+          prev = head_.load(std::memory_order_relaxed);
+          curr = nullptr;
+          break;
+        }
+      }
+
+
+      if(!curr)
+      {
+        continue;
+      }
+
+      assert(curr==cleared);
+
+      type& pair = curr->data_;
+      size_t key_ = pair.first;
+      if(comporator_(key, key_))
+      {
+        return std::make_pair(prev, hzp_c);
+      }
+
+      prev = curr;
+    }
+  }
+
+
+  template<typename U>
+  [[nodiscard]]
+  std::pair<Node*,Hazardous::HZP*> 
+  find_node(const U& key, Node* it = {})
+  {
+    if(!it)
+    {
+      it = head_.load(std::memory_order_relaxed);
+    }
+
+    size_t key_ = std::hash<U>{}(key);
+    auto [prev, hzp] = search_node(key_, it);
+    if(!hzp)
+    {
+      return {};
+    }
+
+    Node* ptr = static_cast<Node*>(hzp->get());
+    if(ptr && (ptr->data_.first == key_))
+    {
+      return std::make_pair(prev,hzp);
+    }
+    hzp->reset();
+    return {};
+  }
+  
+  public:
+
+  [[nodiscard]]
+  Node* data() noexcept
+  {
+    return head_.load(std::memory_order_relaxed);
+  }
+
+
+  [[nodiscard]]
+  Node* back() noexcept
+  {
+    return tail_.load(std::memory_order_relaxed);
+  }
+
+
+  [[nodiscard]]
+  auto begin() noexcept
+  {
+    return order_iterator(head_.load(std::memory_order_relaxed));
+  }
+
+
+  [[nodiscard]]
+  auto end() noexcept
+  {
+    return order_iterator(nullptr);
+  }
+
+
+  void 
+  clear() override
+  {
+    alloc_.clear();
+    void * p = allocate();
+    Node * new_head = ::new(p) Node();
+
+    p = allocate();
+    Node * new_next = ::new(p) Node();
+    new_head->next_.store(new_next, std::memory_order_relaxed);
+
+    Node* head = head_.exchange(new_head, std::memory_order_relaxed);
+    tail_.exchange(new_next, std::memory_order_relaxed);
+    while(head)
+    {
+      Node* next = head->next();
+      alloc_.deallocate(head, sizeof(Node));
+      head = clear_tag(next).second;
+    }
+    counter_.store(0, std::memory_order_release);
+  }
+
+
+  template<typename U>
+  [[nodiscard]]
+  std::optional<T>
+  contain(const U& key, Node * it = {})
+  {
+    Hazardous::HZP* hzp = find_node(key, it).second;
+    if(!hzp)
+    {
+      return {};
+    }
+
+    Node* ptr = static_cast<Node*>(hzp->get());
+    std::optional<T> opt = ptr->data_.second;
+    hzp->reset();
+    return opt;
+  }
+
+
+  template<typename U>
+  [[nodiscard]]
+  bool 
+  find(const U& key, Node* it = {})
+  {
+    Hazardous::HZP* hzp = find_node(key, it).second;
+    if(!hzp)
+    {
+      return false;
+    }
+    hzp->reset();
+    return true;
+  }
+
+
+  template<typename U>
+  Node* 
+  insert(U&& data, Node* it = {}, bool before = false)
+  {
+    if(!it)
+    {
+      it = head_.load(std::memory_order_relaxed);
+    }
+
+    auto u = std::hash<std::remove_cvref_t<U>>{};
+    size_t key = u(std::forward<U>(data));  
+    auto [prev, hzp] = search_node(key, it);
+    if(!prev)
+    {
+      return {};
+    }
+
+    Node* node{};
+    if(hzp == nullptr || before)
+    {
+      node = prev;
+    }
+    else
+    {
+      node = static_cast<Node*>(hzp->get());
+    }
+
+    if(hzp)
+    {
+      hzp->clear();
+      alloc_.deallocate_hazard(hzp);
+    }
+    
+    assert(node);
+
+    Node* tail = tail_.load(std::memory_order_relaxed);
+    Node* next = node->next_.load(std::memory_order_relaxed);
+
+    void* ptr =  alloc_.allocate(sizeof(Node));
+    Node* new_node = ::new(ptr) Node(type{key, std::forward<U>(data)});
+    new_node->next_.store(next, std::memory_order_relaxed);
+
+    if(!node->next_.compare_exchange_strong(next, new_node, std::memory_order_acq_rel))
+    {
+      alloc_.deallocate(new_node, sizeof(Node));
+      return {};
+    }
+    if(node == tail)
+    {
+      while(!tail_.compare_exchange_weak(tail, new_node, std::memory_order_relaxed));
+    }
+    counter_.fetch_add(1, std::memory_order_release);
+  
+    return new_node;
+  }
+
+
+  template<typename U>
+  bool 
+  remove(const U& key, Node* it = {})
+  {
+    if(!it)
+    {
+      it = head_.load(std::memory_order_relaxed);
+    }
+
+    auto[prev,hzp] = find_node(key, it);
+    if(!hzp)
+    {
+      return false;
+    }
+    
+    Node * curr = prev;
+    hzp->clear();
+    alloc_.deallocate_hazard(hzp);
+
+    if(!curr)
+    {
+      return false;
+    }
+
+    Node * next = curr->next_.load(std::memory_order_relaxed);
+    Node * corrupted = corrupt_node(next);
+    if(!curr->next_.compare_exchange_strong(next, corrupted, std::memory_order_relaxed))
+    {
+      return false;
+    }
+    
+    tail_.compare_exchange_strong(next, prev, std::memory_order_relaxed);
+    counter_.fetch_sub(1, std::memory_order_release);
+    return true;
+  }
+
+
+  [[nodiscard]]
+  LF_OrderList 
+  cut(Node* it) noexcept
+  {
+    FreeList_t old_list = FreeList_t::cut(it);
+    LF_OrderList temp{};
+    temp.merge(std::move(old_list));
+    return std::move(temp);
+  }
+
+};
+
