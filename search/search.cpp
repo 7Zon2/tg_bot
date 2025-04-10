e64dc2bfe894f94f94c7a85ee617865a3d435d01
7Zon2 <manukovskiy.1997@gmail.com>
2025-04-10 06:28:58 +0300

    mimmap

--- search/search.cpp
+++ search/search.cpp
@@ -1,16 +1,21 @@
 #include "boost/asio/any_io_executor.hpp"
 #include "boost/asio/detached.hpp"
-#include "boost/asio/impl/co_spawn.hpp"
+#include "boost/asio/execution_context.hpp"
 #include "boost/asio/io_context.hpp"
 #include "boost/asio/ssl/context.hpp"
-#include "boost/asio/this_coro.hpp"
-#include "boost/beast.hpp"
-#include "boost/asio/co_spawn.hpp"
 #include "boost/asio/ssl.hpp"
-#include "boost/asio/awaitable.hpp"
 #include "boost/asio/use_awaitable.hpp"
+#include "boost/beast.hpp"
+#include "boost/iostreams/device/mapped_file.hpp"
+#include "boost/asio/co_spawn.hpp"
+#include "boost/asio/awaitable.hpp"
+#include "boost/beast/http/message.hpp"
+#include "boost/beast/http/string_body.hpp"
+#include "boost/beast/http/verb.hpp"
 #include "json_head.hpp"
 #include "print.hpp"
+#include <cstring>
+#include <fstream>
 
 
 namespace beast = boost::beast;         // from <boost/beast.hpp>
@@ -68,7 +73,7 @@ class session_interface
   virtual handshake()
   {
     print("Handshake...\n");
-    // Perform the SSL handshake
+    
     co_await stream_->async_handshake(ssl::stream_base::client);
     co_return;
   }
@@ -116,7 +121,7 @@ class session_interface
   {
     print("\nserver endpoints:\n");
     print("number of endpoints:", res.size(),"\n");
-    for(auto && i : res)
+    for(auto & i : res)
     {
         print("\nendpoint:\n");
         print("service_name:",i.service_name(),"\n");
@@ -124,6 +129,23 @@ class session_interface
         print_endpoint(i.endpoint());
     }
   }
+
+
+  static void 
+  print_response(auto&& res)
+  {
+    print("Response:\n\n====================================================================================\n");
+    for(auto&& i : res)
+    {
+      print
+      (
+        "field name: ",  i.name_string(),"\t",
+        "field value: ", i.value(),"\n"
+      );
+    }
+    print(res,"\n=========================================================================================\n");
+  }
+
 
 
   [[nodiscard]]
@@ -136,6 +158,60 @@ class session_interface
     return ctx;
   }
 
+
+  [[nodiscard]]
+  static 
+  http::request<http::string_body>
+  make_header
+  (
+    http::verb verb,
+    json::string host,
+    json::string target,
+    json::string user_agent = "Raven-Fairy"
+  )
+  {
+    http::request<http::string_body> req;
+    req.method(verb);
+    req.set(http::field::host, host);
+    req.set(http::field::user_agent, user_agent);
+    req.target(target);
+    return req;
+  }
+
+
+  static void 
+  prepare_multipart
+  (
+    http::request<http::string_body>& req,
+    json::string content_type,
+    json::string data,
+    Pars::optstr encoding = {},
+    json::string boundaries = "Fairy"
+  )
+  {
+    static const json::string  crlf{"\r\n"};
+
+    json::string head_content{"multipart/form-data; boundary="}; head_content += boundaries;
+    req.set(http::field::content_type, head_content);
+
+    if(encoding.has_value())
+    {
+      req.set(http::field::accept_encoding, encoding.value());
+    }
+
+    json::string body{}; 
+    body += "--"; body+=boundaries;                 body += crlf;
+    
+    body+="Content-Disposition: form-data;";        body += crlf;
+    body+="Content-Type:"; body += content_type;    body += crlf; body += crlf;
+
+    body+=std::move(data); body += crlf;
+
+    body += "--"; body += boundaries; body += "--"; body += crlf;
+
+    req.body() = std::move(body);
+  }
+
   protected:
 
   session_interface
@@ -144,7 +220,7 @@ class session_interface
    json::string port, 
    int version,
    ssl::context ctx,
-   net::any_io_executor& executor
+   net::any_io_executor executor
   )
   noexcept :
   host_(host),
@@ -163,11 +239,11 @@ class session_interface
   std::shared_ptr<Derived> 
   make_session
   (
-    net::any_io_executor& ex,
+    net::any_io_executor ex,
     json::string host,
     json::string port,
     int version,
-    std::optional<ssl::context> ctx,
+    std::optional<ssl::context> ctx = {},
     Args&&...args
   )
   {
@@ -198,6 +274,20 @@ class session_interface
   }
 
 
+  net::awaitable<void>
+  virtual 
+  req_res(http::request<http::string_body> req)
+  {
+    size_t write_data = co_await http::async_write(*stream_, std::move(req), net::use_awaitable);
+    print("\nwrite data:", write_data, "\n");
+
+    boost::beast::flat_buffer buffer{};
+    http::response<http::string_body> resp;
+    size_t read_data = co_await http::async_read(*stream_, buffer, resp, net::use_awaitable);
+    print("read data:", read_data,"\n");
+    print_response(resp);
+  }
+
   virtual ~session_interface(){}
 };
 
@@ -205,13 +295,15 @@ class session_interface
 
 class Searcher : public session_interface
 {
+  public:
+
   Searcher
   (
    json::string host,
    json::string port, 
    int version,
    ssl::context ctx,
-   net::any_io_executor& executor
+   net::any_io_executor executor
   )
   noexcept :
   session_interface
@@ -225,9 +317,40 @@ class Searcher : public session_interface
   {
   }
 
-    public:
+  net::awaitable<void>
+  request_upload_image
+  (boost::iostreams::mapped_file map, json::string fileName)
+  {
+    map = boost::iostreams::mapped_file{fileName.data(), boost::iostreams::mapped_file::readonly};
+    json::string data;
+    data.reserve(map.size());
 
-    static inline json::string host_{"google.com"};
+    for(auto cb = map.const_data(); cb != cb + map.size();cb++)
+    {
+      data.append(cb);
+    }
+
+    auto req = make_header
+      (
+        http::verb::post,
+        host_,
+        "/searchbyimage/upload"
+      );
+
+    prepare_multipart
+    (
+      req,
+      "application/jpeg",
+      std::move(data),
+      "gzip, deflate, br, zstd"
+    );
+
+    co_await req_res(std::move(req));
+  }
+
+  public:
+
+  static inline json::string host_{"google.com"};
 };
 
 
@@ -238,8 +361,10 @@ int main()
   try
   {
     net::io_context ioc;
-    auto session = session_interface::make_session<Searcher>(ioc, Searcher::host_, "5000",11,{});
+    auto session = session_interface::make_session<Searcher>(ioc.get_executor(), Searcher::host_, "443",11);
     net::co_spawn(ioc, session->run(), net::detached);
+    net::co_spawn(ioc, session->request_upload_image(boost::iostreams::mapped_file{}, "/home/zon/Downloads/Kiki.jpg"), net::detached);
+    ioc.run();
   }
   catch(const std::exception& ex)
   {
